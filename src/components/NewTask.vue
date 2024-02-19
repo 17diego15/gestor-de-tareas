3a320e9df21b6b67bd682eac97e7a1813935d541
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-02-19 18:20:08 +0100

    First commit

---
+++ src/components/NewTask.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const emits = defineEmits(['add'])
+const name = ref('')
+
+function onAdd(){
+    emits('add', 'name.value')
+    name.value = ''
+}
+
+function changeText(e: Event){
+    name.value = (e.target as HTMLInputElement).value
+}
+</script>
+
+<template>
+    <div>
+        <input v-model="name" type="text" name="" id="">
+        <button @click="onAdd">Add</button>
+    </div>
+</template>
